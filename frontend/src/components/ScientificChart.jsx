4d82013695b995f85066fd91d029ae28b2702d97
RugmaBiju <rugmabiju@gmail.com>
2026-03-23 12:26:37 +0530

    Added chart component

---
+++ frontend/src/components/ScientificChart.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+function ScientificChart() {
+  return (
+    <div>
+      <h3>Ocean Data Chart</h3>
+      <p>Chart will render here</p>
+    </div>
+  )
+}
+
+export default ScientificChart
