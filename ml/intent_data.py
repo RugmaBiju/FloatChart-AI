b356000999b02f2571ff2774e82958ff6c73dd9f
RugmaBiju <rugmabiju@gmail.com>
2026-04-15 18:13:07 +0530

    Local changes

---
+++ ml/intent_data.py
@@ -0,0 +1,22 @@
+intent_samples = {
+    "AVG_TEMP": [
+        "average sea surface temperature",
+        "mean sea temperature",
+        "what is the average temperature"
+    ],
+    "TEMP_TREND": [
+        "show temperature trend",
+        "temperature over time",
+        "plot temperature change"
+    ],
+    "MAX_SEA_LEVEL": [
+        "highest sea level",
+        "maximum sea level",
+        "sea level peak"
+    ],
+    "SUMMARY": [
+        "give dataset summary",
+        "describe the data",
+        "overview of dataset"
+    ]
+}
